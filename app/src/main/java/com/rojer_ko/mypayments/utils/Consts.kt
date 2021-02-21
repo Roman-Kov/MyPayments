e65c370e5f9c935922e1a951f45d87d9066421f0
Roman-Kov <rojer_ko@bk.ru>
2021-02-22 02:15:00 +0300

    Добавил utils: константы, вспомогательные классы и расширения

---
+++ app/src/main/java/com/rojer_ko/mypayments/utils/Consts.kt
@@ -0,0 +1,19 @@
+package com.rojer_ko.mypayments.utils
+
+sealed class Consts {
+
+    class Result {
+        companion object {
+
+            const val OK = "ok"
+        }
+    }
+
+    class Error{
+        companion object {
+
+            const val LOGIN_PASSWORD_WRONG = "Login or password wrong"
+            const val TOKEN_IS_BLANK = "Token is blank"
+        }
+    }
+}
---
+++ app/src/main/java/com/rojer_ko/mypayments/utils/Extentions.kt
@@ -0,0 +1,8 @@
+package com.rojer_ko.mypayments.utils
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.showToast(message: String) {
+    Toast.makeText(this.context, message, Toast.LENGTH_LONG).show()
+}
