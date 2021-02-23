9187513ba5655a4e44ac700dce74cf53d77ec272
Roman-Kov <rojer_ko@bk.ru>
2021-02-24 00:32:27 +0300

    Добавил отображение диалога при выходе из программы

--- app/src/main/java/com/rojer_ko/mypayments/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/rojer_ko/mypayments/presentation/ui/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.rojer_ko.mypayments.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.rojer_ko.mypayments.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+
+        val destination = nav_host_fragment.findNavController().currentDestination
+        destination?.let {
+            if (it.id == R.id.paymentsFragment) {
+                showExitDialog()
+            } else {
+                super.onBackPressed()
+            }
+        }
+    }
+
+    private fun showExitDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage(getString(R.string.exit_from_app))
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                super.onBackPressed()
+            }
+            setNegativeButton(getString(R.string.no)) { _, _ ->
+            }
+            setCancelable(true)
+        }.create().show()
     }
 }
